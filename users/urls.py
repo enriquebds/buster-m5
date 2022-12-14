d6a025cf48ed7989849f532bbd0b1860e277cfcf
Enrique <enrique.barbosasilva@gmail.com>
2022-12-14 02:57:20 -0300

    refactor: code-changed

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("users/", views.RegisterView.as_view()),
     path("users/login/", views.LoginView.as_view()),
+    path("users/<user_id>/", views.UserDetailView.as_view()),
 ]
