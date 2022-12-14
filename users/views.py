4907e10499f3cec67f0b167edf405f15d45df509
Enrique <enrique.barbosasilva@gmail.com>
2022-12-14 02:57:06 -0300

    refactor: code-changed

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from .models import User
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.permissions import Permission
 
 class RegisterView(APIView):
     
@@ -19,3 +21,14 @@ class RegisterView(APIView):
 class LoginView(TokenObtainPairView):
     ...
     
+class UserDetailView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [Permission]
+    
+
+    def get(self, request: Request, user_id: int) -> Response:
+        user = User.objects.get(id=user_id)
+        self.check_object_permissions(request,user)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK) 
